9a6a25418ad5e33852ea634f1e9c218073274351
TomosZZZ <okninski.t2004@gmail.com>
2024-09-26 15:17:21 +0200

    refactor: Change login and signup schemas and change file names

---
+++ features/auth/schemas/signup-schema.ts
@@ -0,0 +1,14 @@
+import { string, object } from "zod";
+
+export const signUpFormSchema = object({
+  username: string({ required_error: "Username is required" }).min(3, {
+    message: "Username must be at least 3 characters long",
+  }),
+  email: string({ required_error: "Email is required" })
+    .min(1, "Email is required")
+    .email("Invalid email"),
+  password: string({ required_error: "Password is required" })
+    .min(1, "Password is required")
+    .min(8, "Password must be more than 8 characters")
+    .max(32, "Password must be less than 32 characters"),
+});

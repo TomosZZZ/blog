847230e1167ef652f67693c12b26740b438c56e1
TomosZZZ <okninski.t2004@gmail.com>
2024-10-04 08:07:53 +0200

    feat: Configure auth.ts file for Credentials provider

--- features/auth/config/auth.ts
+++ features/auth/config/auth.ts
@@ -2,7 +2,51 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/prisma";
 
+import Credentials from "next-auth/providers/credentials";
+import { loginSchema } from "../schemas";
+import { UserRepository } from "@/features/user/repository";
+
+import bcrypt from "bcryptjs";
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  pages: {
+    signIn: "/auth/signin",
+  },
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        const { success, data } = loginSchema.safeParse(credentials);
+
+        if (!success || !data) {
+          return null;
+        }
+
+        const { email, password } = data;
+        const userRepository = new UserRepository();
+
+        if (!success || !data) {
+          return null;
+        }
+
+        const user = await userRepository.getUserByEmail(email);
+
+        if (!user || !user.password) {
+          return null;
+        }
+
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordsMatch) {
+          return null;
+        }
+
+        return user;
+      },
+    }),
+  ],
 });

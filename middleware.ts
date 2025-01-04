1906f7ebb219f935a2d10ed2b546280a1d028394
TomosZZZ <okninski.t2004@gmail.com>
2025-01-04 15:18:11 +0100

    feat: Add route protection with middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,34 @@
 import NextAuth from "next-auth";
 import { authConfig } from "@/features/auth/config/auth.config";
+import { ADMIN_ROUTES, AUTH_ROUTES, DEFAULT_LOGIN_REDIRECT } from "./features";
+import { NextResponse } from "next/server";
+import { Role } from "@prisma/client";
 
-export const { auth: middleware } = NextAuth(authConfig);
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const { nextUrl, auth } = req;
+  const isLoggedIn = !!auth;
+  const isAdmin = auth?.accessToken.role === Role.ADMIN;
+
+  const isAuthRoute = AUTH_ROUTES.includes(nextUrl.pathname);
+  const isAdminRoute = ADMIN_ROUTES.includes(nextUrl.pathname);
+  const isPublicRoute = !isAdminRoute && !isAuthRoute;
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return;
+  }
+  if (isAdminRoute && !isAdmin) {
+    return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+  }
+  if (!isPublicRoute && !isLoggedIn) {
+    return NextResponse.redirect(new URL("/auth/login", nextUrl));
+  }
+  return;
+});
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};

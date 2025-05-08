dadf3d514acbe3ce76d69fd6a8edc2b7e64b9ca6
TomosZZZ <okninski.t2004@gmail.com>
2025-05-08 12:33:19 +0200

    feat: Create a function to get text for post description from tip tap JSON data.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,24 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { IGNORED_TYPES, TiptapNode } from "./types";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const extractTextFromTipTapJSON = (node: TiptapNode): string => {
+  if (!node || IGNORED_TYPES.has(node.type)) return "";
+
+  let text = "";
+  if (node.type === "text" && node.text) {
+    text += node.text;
+  }
+
+  if (Array.isArray(node.content)) {
+    for (const child of node.content) {
+      text += extractTextFromTipTapJSON(child) + " ";
+    }
+  }
+
+  return text.trim();
+};

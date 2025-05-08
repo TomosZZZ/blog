884f20134276600192f64979fd372ee56835a03c
TomosZZZ <okninski.t2004@gmail.com>
2025-05-08 12:34:04 +0200

    feat: Create blog component and add add fetching posts and getting post description

--- features/blog/components/blog/blog.tsx
+++ features/blog/components/blog/blog.tsx
@@ -1,4 +1,33 @@
+"use client";
+import { useGetPosts } from "@/features/blog/api";
+import { extractTextFromTipTapJSON } from "@/lib/utils";
+import { useEffect, useState } from "react";
+
 export const Blog = () => {
+  const { data, isLoading, isError } = useGetPosts();
+  const [postDescriptions, setPostDescriptions] = useState<string[]>([]);
+  const [areDescriptionsLoading, setDescriptionsLoading] = useState(false);
+
+  useEffect(() => {
+    if (!data) return;
+
+    setDescriptionsLoading(true);
+
+    const descriptions: string[] = [];
+
+    for (const post of data) {
+      try {
+        const contentJson = JSON.parse(post.content);
+        const text = extractTextFromTipTapJSON(contentJson);
+        descriptions.push(text);
+      } catch (e) {
+        descriptions.push("");
+      }
+    }
+    setPostDescriptions(descriptions);
+    setDescriptionsLoading(false);
+  }, [data]);
+
   return (
     <div className="text-white">
       <h1 className="text-4xl font-bold">Blog</h1>

4cf97540d28170866218b39c1afedc044ac7fbfb
TomosZZZ <okninski.t2004@gmail.com>
2025-05-27 14:06:11 +0200

    refactor: Change error messages to polish, make zod responsible for file size validation

--- features/blog/components/modal/create-post-modal.tsx
+++ features/blog/components/modal/create-post-modal.tsx
@@ -52,9 +52,6 @@ export const CreatePostModal = ({
 
   const onSubmit = async (values: z.infer<typeof createPostFormSchema>) => {
     try {
-      if (values.thumbnail.size > 1024 * 1024) {
-        throw new Error("Plik przekracza maksymalny rozmiar 1MB");
-      }
       const thumbnailBase64Response = await convertImageToBase64(
         values.thumbnail
       );

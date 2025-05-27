17675d2af0a2105472ac42202200c3839a25cb4b
TomosZZZ <okninski.t2004@gmail.com>
2025-05-27 14:07:42 +0200

    feat: Create postCard component and add it to blog page

--- features/blog/components/blog/blog.tsx
+++ features/blog/components/blog/blog.tsx
@@ -2,11 +2,15 @@
 import { useGetPosts } from "@/features/blog/api";
 import { extractTextFromTipTapJSON } from "@/lib/utils";
 import { useEffect, useState } from "react";
+import { PostCard } from "./post-card";
+import { Silkscreen } from "next/font/google";
+
+const silkscreen = Silkscreen({ weight: "700", subsets: ["latin"] });
 
 export const Blog = () => {
   const { data, isLoading, isError } = useGetPosts();
   const [postDescriptions, setPostDescriptions] = useState<string[]>([]);
-  const [areDescriptionsLoading, setDescriptionsLoading] = useState(false);
+  const [areDescriptionsLoading, setDescriptionsLoading] = useState(true);
 
   useEffect(() => {
     if (!data) return;
@@ -30,7 +34,25 @@ export const Blog = () => {
 
   return (
     <div className="text-white">
-      <h1 className="text-4xl font-bold">Blog</h1>
+      <h1
+        className={`text-4xl font-bold text-center my-5 md:my-10 ${silkscreen.className}`}
+      >
+        Blog
+      </h1>
+      <div className="flex flex-wrap gap-x-6 gap-y-8 justify-center my-4">
+        {!isLoading &&
+          !isError &&
+          !areDescriptionsLoading &&
+          data?.map((post, index) => (
+            <PostCard
+              date={post.createdAt.toString().split("T")[0]}
+              key={post.id}
+              title={post.title}
+              description={postDescriptions[index]}
+              thumbnail={post.thumbnail}
+            />
+          ))}
+      </div>
     </div>
   );
 };

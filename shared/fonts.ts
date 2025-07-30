d73af0f70df4dc4a5b0cd4fd1167b9170a3e321d
TomosZZZ <okninski.t2004@gmail.com>
2025-07-30 14:58:27 +0200

    feat: Add new font - inter

--- shared/fonts.ts
+++ shared/fonts.ts
@@ -1,6 +1,10 @@
-import { Silkscreen } from "next/font/google";
+import { Silkscreen, Inter } from "next/font/google";
 
 export const silkscreen = Silkscreen({
   weight: "700",
+  subsets: ["latin"],
+});
+
+export const inter = Inter({
   subsets: ["latin"],
 });

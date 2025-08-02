9ec1e5ebe0b6e61d1592b2e4efb30bc3f09e3d36
TomosZZZ <okninski.t2004@gmail.com>
2025-08-02 14:43:01 +0200

    feat: Enhance styling of loader component. Add text prop to it

--- shared/components/Loader.tsx
+++ shared/components/Loader.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { BarLoader } from "react-spinners";
 import { silkscreen } from "../fonts";
 
-export const Loader = () => {
+interface LoaderProps {
+  text?: string;
+}
+
+export const Loader = ({ text = "Loading..." }: LoaderProps) => {
   return (
     <div className="text-center flex flex-col items-center gap-2">
-      <p className={`${silkscreen.className} text-violet-400 `}>Loading...</p>
-      <BarLoader color="#5821b6" height={5} width={150} />
+      <p className={`${silkscreen.className} text-violet-400`}>{text}</p>
+      <BarLoader color="#8b5cf6" height={5} width={150} />{" "}
     </div>
   );
 };

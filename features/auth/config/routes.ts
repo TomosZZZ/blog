a3a0c50d2aff72b8166e44dcf82e6b1dd9ff846c
TomosZZZ <okninski.t2004@gmail.com>
2025-09-23 10:41:20 +0200

    feat: Add /admin, /admin/manage-posts to admin routes

--- features/auth/config/routes.ts
+++ features/auth/config/routes.ts
@@ -1,3 +1,3 @@
-export const ADMIN_ROUTES = ["/blog/new-post"];
+export const ADMIN_ROUTES = ["/blog/new-post", "/admin", "/admin/manage-posts"];
 export const AUTH_ROUTES = ["/auth/login", "/auth/signup"];
 export const DEFAULT_LOGIN_REDIRECT = "/blog";

8babd1c8c7903847124c39a566c0788808252921
TomosZZZ <okninski.t2004@gmail.com>
2025-10-06 20:27:43 +0200

    feat: Create hook for post edition

--- features/blog/api/index.ts
+++ features/blog/api/index.ts
@@ -2,3 +2,4 @@ export * from "./get-posts";
 export * from "./create-post";
 export * from "./get-post-by-id";
 export * from "./delete-post";
+export * from "./update-post";
---
+++ features/blog/api/update-post/use-update-post.tsx
@@ -0,0 +1,37 @@
+import { PostDTO } from "@/features/blog/types/post";
+import { useMutation } from "@tanstack/react-query";
+
+interface UpdatePostParams {
+  postId: string;
+  post: Partial<PostDTO>;
+  token: string;
+}
+
+export const useUpdatePost = () => {
+  const updatePost = useMutation({
+    mutationFn: async ({ postId, post, token }: UpdatePostParams) => {
+      const res = await fetch(
+        `http://localhost:8080/api/posts/update/${postId}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(post),
+        }
+      );
+      if (!res.ok) {
+        const text = await res.text();
+        throw new Error(text || "Something went wrong");
+      }
+    },
+    onSuccess: () => {
+      console.log("Post updated successfully");
+    },
+    onError: (error: any) => {
+      console.error("Error updating post:", error.message);
+    },
+  });
+  return updatePost;
+};

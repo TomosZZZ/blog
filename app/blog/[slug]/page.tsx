55532efc848c1f1dc0d4f023d38ce7f27e50bb37
TomosZZZ <okninski.t2004@gmail.com>
2025-10-07 14:36:18 +0200

    feat: Make post page rendered statically on server

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,16 +1,36 @@
-import { Post } from "@/features/blog";
+import { Post } from "@/features";
+import { getPostById, getPosts } from "@/features/blog/api";
+import { Post as PostModel } from "@/features/blog/types/post";
+import { notFound } from "next/navigation";
 import React from "react";
 
 type Props = {
   params: { slug: string };
 };
 
-const PostPage = ({ params }: Props) => {
+type PostParams = {
+  slug: string;
+};
+
+export async function generateStaticParams(): Promise<PostParams[]> {
+  const posts = await getPosts();
+
+  return posts.map((post) => ({ slug: post.slug }));
+}
+
+const PostPage = async ({ params }: Props) => {
   const { slug } = params;
   const postId = slug.split("~")[1];
+
+  const post = await getPostById(postId);
+
+  if (!post) {
+    notFound();
+  }
+
   return (
     <div className="flex items-center justify-center">
-      <Post postId={postId} />
+      <Post initialData={post} />
     </div>
   );
 };
--- features/blog/components/blog/post/post.tsx
+++ features/blog/components/blog/post/post.tsx
@@ -2,47 +2,63 @@
 
 import { useGetPostById } from "@/features/blog/api";
 import { EditorContent, useEditor } from "@tiptap/react";
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import { RichTextEditorExtensions } from "../../text-editor";
 import { default as NextImage } from "next/image";
 import { PostSkeleton } from "./post-skeleton";
+import { Post as PostModel } from "@/features/blog/types/post";
 
 type Props = {
-  postId: string;
+  initialData: PostModel;
 };
 
-export const Post = ({ postId }: Props) => {
-  const { data, isLoading, isError, error } = useGetPostById(postId);
+export const Post = ({ initialData }: Props) => {
+  const { data, isLoading, isError, error } = useGetPostById(
+    initialData.id,
+    initialData
+  );
+
+  const post = data || initialData;
+
+  const parsedContent = useMemo(() => {
+    if (post?.content) {
+      try {
+        return JSON.parse(post.content);
+      } catch (e) {
+        console.error("Failed to parse editor content:", e);
+        return "";
+      }
+    }
+    return "";
+  }, [post?.content]);
 
   const editor = useEditor({
     extensions: RichTextEditorExtensions,
-    content: "",
+    content: parsedContent,
     editable: false,
   });
 
   useEffect(() => {
-    if (!editor || !data?.content) {
+    if (!editor || !parsedContent) {
       return;
     }
 
     try {
-      const newContent = JSON.parse(data.content);
-
       const currentContent = editor.getJSON();
 
-      if (JSON.stringify(currentContent) !== JSON.stringify(newContent)) {
-        editor.commands.setContent(newContent, false);
+      if (JSON.stringify(currentContent) !== JSON.stringify(parsedContent)) {
+        editor.commands.setContent(parsedContent, false);
       }
     } catch (e) {
       console.error("Failed to parse or set editor content:", e);
     }
-  }, [data, editor]);
+  }, [parsedContent, editor]);
 
-  if (isLoading) {
+  if (isLoading || !post) {
     return <PostSkeleton />;
   }
 
-  if (isError || !data) {
+  if (isError || !post) {
     return (
       <div className="text-red-500 text-center py-20">
         <h2>Something went wrong!</h2>
@@ -56,13 +72,13 @@ export const Post = ({ postId }: Props) => {
       <div className="flex flex-col gap-8">
         <div>
           <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-8 text-center">
-            {data.title}
+            {post.title}
           </h1>
 
           <div className="w-full relative aspect-video overflow-hidden rounded-lg shadow-lg">
             <NextImage
-              src={data.thumbnail}
-              alt={`Thumbnail for ${data.title}`}
+              src={post.thumbnail}
+              alt={`Thumbnail for ${post.title}`}
               fill
               className="object-cover"
               sizes="(max-width: 768px) 100vw, (max-width: 1280px) 80vw, 1200px"

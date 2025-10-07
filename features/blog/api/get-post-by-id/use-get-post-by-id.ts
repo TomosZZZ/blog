2e31da4f7428b291c07ead4854a121c7099f76b3
TomosZZZ <okninski.t2004@gmail.com>
2025-10-07 14:35:40 +0200

    refactor: Extract mutation function and export them for server components

--- features/blog/api/get-post-by-id/use-get-post-by-id.ts
+++ features/blog/api/get-post-by-id/use-get-post-by-id.ts
@@ -1,23 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 import { Post } from "../../types/post";
 
-export const useGetPostById = (postId: string) => {
+export const getPostById = async (postId: string) => {
+  const response = await fetch(`http://localhost:8080/api/posts/get/${postId}`);
+  if (response.status === 404) {
+    throw new Error("Post was not found");
+  }
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
+  const data: Post | null = await response.json();
+  return data;
+};
+
+export const useGetPostById = (postId: string, initialData?: Post) => {
   const getPostByIdQuery = useQuery({
     queryKey: ["post", postId],
-    queryFn: async () => {
-      const response = await fetch(
-        `http://localhost:8080/api/posts/get/${postId}`
-      );
-      if (response.status === 404) {
-        throw new Error("Post nie zostal znaleziony");
-      }
-      if (!response.ok) {
-        throw new Error("Coś nie tak.");
-      }
-      const data: Post | null = await response.json();
-      return data;
-    },
+    queryFn: () => getPostById(postId),
     enabled: !!postId,
+    initialData: initialData,
   });
 
   return getPostByIdQuery;
--- features/blog/api/get-posts/use-get-posts.tsx
+++ features/blog/api/get-posts/use-get-posts.tsx
@@ -1,22 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 import { Post } from "../../types/post";
 
+export const getPosts = async () => {
+  const res = await fetch("http://localhost:8080/api/posts/get", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (!res.ok) {
+    throw new Error("Something went wrong while fetching posts");
+  }
+  const data = (await res.json()) as Post[];
+  return data;
+};
+
 export const useGetPosts = () => {
   const getPostsQuery = useQuery({
     queryKey: ["posts"],
-    queryFn: async () => {
-      const res = await fetch("http://localhost:8080/api/posts/get", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      if (!res.ok) {
-        throw new Error("Something went wrong while fetching posts");
-      }
-      const data = (await res.json()) as Post[];
-      return data;
-    },
+    queryFn: getPosts,
   });
   return getPostsQuery;
 };

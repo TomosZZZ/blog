e862b24b2c80b6f1391654c0ab3710f4224f73bc
TomosZZZ <okninski.t2004@gmail.com>
2025-10-16 20:38:51 +0200

    feat: Add endpoint for fetching users

---
+++ features/user/api/get-users/use-get-users.ts
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import { User } from "@prisma/client";
+
+const getUsers = async (authHeader: string) => {
+  const res = await fetch("http://localhost:8080/api/users/get", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: authHeader,
+    },
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch users");
+  }
+  const data = (await res.json()) as User[];
+  return data;
+};
+
+export const useGetUsers = (token: string) => {
+  const getUsersQuery = useQuery({
+    queryKey: ["users"],
+    queryFn: () => getUsers(`Bearer ${token}`),
+  });
+  return getUsersQuery;
+};

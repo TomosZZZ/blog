274083710798cee7996db709e38aac01cea0191b
TomosZZZ <okninski.t2004@gmail.com>
2025-10-23 17:59:17 +0200

    feat: Add endpoint for deleting user

---
+++ features/user/api/delete-user/use-delete-user.ts
@@ -0,0 +1,38 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+export const useDeleteUser = () => {
+  const queryClient = useQueryClient();
+  const deleteUserMutation = useMutation({
+    mutationFn: async ({
+      userId,
+      token,
+    }: {
+      userId: string;
+      token: string;
+    }) => {
+      const res = await fetch(
+        `http://localhost:8080/api/users/delete/${userId}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (!res.ok) {
+        const text = await res.text();
+        throw new Error(text || "Something went wrong");
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["users"] });
+      toast.success("User deleted successfully");
+    },
+    onError: (error) => {
+      toast.error(error.message || "Failed to delete user");
+    },
+  });
+  return deleteUserMutation;
+};
